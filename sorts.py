27c6d974dfed8c3e65474dbe07d626b54ad38b45
cs-inojosa <cristianinojosa@gmail.com>
2019-09-26 13:31:37 -0400

    pls work

--- sorts.py
+++ sorts.py
@@ -20,12 +20,36 @@ def insertion_sort(array:[int]):
 ''' MERGE SORT '''
 
 def merge_sort(A:[int], p:int, r:int):
-	if r-p < 2:
-		return A[p]
+	if r-p > 2:
+		q = (p + r) // 2
 
-	q = (p + r) // 2
+		merge_sort(A, p, q)
+		merge_sort(A, q, r)
 
-	merge_sort(A, p, q)
-	merge_sort(A, q, r)
+		merge(A, p, q, r)
 
-	merge(A, p, q, r)
+def merge( A: [int], p: int, q: int , r: int):
+	
+	Left, Right, n , m = [], [], q-p, r-q
+
+	for i in range (n):
+		Left.append( A[p+i])
+
+	for i in range (m):
+		Right.append( A[q+i])
+
+	Left.append( float("inf") )
+	Right.append( float("-inf") )
+	i, j = 0, 0
+
+	for k in range(p, r):
+		if (Left[i] <= Right[j]):
+			A[k]= Left[i]
+			i+=1
+		else:
+			A[k]=Right[j]
+			j+=1
+
+A = [3, 2, 1, 0]
+merge_sort(A, 0, len(A))
+print(A)
